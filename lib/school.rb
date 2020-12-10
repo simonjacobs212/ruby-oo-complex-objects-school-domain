ac99a7e5ac0cf39ff10af7eeb311a73aa8386122
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-12-09 21:23:08 -0500

    commit tree 0d99a869ccf22c9fba7f1c688b3f6e3202f073a7

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
+require 'pry'
+
 # code here!
+
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        if @roster[grade]
+        @roster[grade] << name
+        else
+        @roster[grade] = name.split(", ")
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each_with_object({}) {|(grade, students), new_hash| students.sort! && new_hash[grade] = students}
+    end
+end
